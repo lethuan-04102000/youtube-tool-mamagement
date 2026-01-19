5930a3b4a8c2f1bca8b010ad8abb7a7f15365a99
thuanlv <thedanh0410@gmail.com>
2026-01-19 15:12:19 +0700

    [watching] auto view and subcribe channel ytb

--- src/routes/v1/watch.routes.js
+++ src/routes/v1/watch.routes.js
@@ -4,14 +4,19 @@ const watchController = require('../../controllers/watch.controller');
 
 /**
  * POST /api/v1/watch/video
+ * POST /api/v1/watch
  * Watch a YouTube video with multiple tabs
  * 
  * Body:
  * - videoUrl: YouTube video URL (required)
- * - tabs: Number of tabs to open (default: 10)
+ * - tabs: Number of tabs to open (default: 1)
  * - duration: Duration to watch in seconds (default: 30)
- * - useAccounts: Use logged in accounts (default: false)
+ * - useAccounts: Use logged in accounts with channels from DB (default: false)
+ * - humanBehavior: Enable human-like behavior (default: true)
+ * - randomDuration: Use random duration (default: false)
+ * - autoSubscribe: Auto subscribe to channel (default: false)
  */
 router.post('/video', (req, res) => watchController.watchVideo(req, res));
+router.post('/', (req, res) => watchController.watchVideo(req, res));
 
 module.exports = router;
